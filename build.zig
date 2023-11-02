e9966520fba06a539ccdd3a71006918ae23566e4
holzmanj <jesseholzman96@gmail.com>
2023-11-02 11:08:27 -0400

    Rework build.zig (copying the way it's done in raylib)

--- build.zig
+++ build.zig
@@ -1,17 +1,42 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
-    const lib = b.addStaticLibrary(.{
+pub fn addFastNoiseLite(
+    b: *std.Build,
+    target: std.zig.CrossTarget,
+    optimize: std.builtin.OptimizeMode,
+) *std.Build.CompileStep {
+    const fnl = b.addStaticLibrary(.{
         .name = "FastNoiseLite",
         .target = target,
         .optimize = optimize,
     });
 
-    lib.addIncludePath(.{ .path = "src" });
-    lib.addCSourceFile(.{ .file = "src/FastNoiseLite.c", .flags = &.{ "-std=c99", "-fno-sanitize=undefined" } });
+    fnl.linkLibC();
+    fnl.addIncludePath(.{ .path = srcdir ++ "src" });
+    fnl.addCSourceFiles(
+        &.{
+            srcdir ++ "/src/FastNoiseLite.c",
+        },
+        &.{
+            "-std=c99",
+            "-fno-sanitize=undefined",
+        },
+    );
+
+    return fnl;
+}
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib = addFastNoiseLite(b, target, optimize);
 
     b.installArtifact(lib);
 }
+
+const srcdir = struct {
+    fn getSrcDir() []const u8 {
+        return std.fs.path.dirname(@src().file).?;
+    }
+}.getSrcDir();
